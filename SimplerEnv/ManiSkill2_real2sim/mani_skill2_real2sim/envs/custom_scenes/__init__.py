5823ea12558b0310433d431f869850b4c5801b84
HITQHX <3175547616@qq.com>
2026-03-26 20:21:37 +0800

    Omit the data/ (3D Digital Assets) in ManiSkill2_real2sim

---
+++ SimplerEnv/ManiSkill2_real2sim/mani_skill2_real2sim/envs/custom_scenes/__init__.py
@@ -0,0 +1,7 @@
+from . import grasp_single_in_scene
+from . import move_near_in_scene
+from . import open_drawer_in_scene
+from . import put_on_in_scene
+from . import place_in_closed_drawer_in_scene
+from . import situated_instruction
+from . import free_form_instruction
